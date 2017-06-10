84ddc63ac1042240270d923c94c91bfe1314ae55
Thomas Andolf <thomas.andolf@gmail.com>
2017-06-10 19:48:05 +0200

    switched from ogm.properties to java based driver config

---
+++ lift-app/src/main/java/se/andolf/configuration/DriverConfig.java
@@ -0,0 +1,56 @@
+package se.andolf.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Thomas on 2017-06-10.
+ */
+@Component
+@ConfigurationProperties(prefix = "driver.config")
+public class DriverConfig {
+    private String uri;
+    private String name;
+    private String index;
+    private final Pool pool = new Pool();
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(final String uri) {
+        this.uri = uri;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(final String index) {
+        this.index = index;
+    }
+
+    public Pool getPool() {
+        return pool;
+    }
+
+    public static class Pool {
+        private int size;
+
+        public int getSize() {
+            return size;
+        }
+
+        public void setSize(final int size) {
+            this.size = size;
+        }
+    }
+}
--- lift-app/src/main/java/se/andolf/configuration/SessionConfig.java
+++ lift-app/src/main/java/se/andolf/configuration/SessionConfig.java
@@ -3,6 +3,7 @@ package se.andolf.configuration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
@@ -20,14 +21,30 @@ public class SessionConfig {
 
     private static Log LOG = LogFactory.getLog(ExerciseService.class);
 
+    @Autowired
+    private DriverConfig driverConfig;
+
     @Bean
     public SessionFactory sessionFactory(){
         LOG.debug("Loading session factory");
-        return new SessionFactory("se.andolf");
+        return new SessionFactory(getConfiguration(), "se.andolf");
     }
 
     @Bean
     public Neo4jTransactionManager transactionManager() {
         return new Neo4jTransactionManager(sessionFactory());
+    }
+
+    @Bean
+    public org.neo4j.ogm.config.Configuration getConfiguration() {
+        org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
+        config.driverConfiguration()
+                .setDriverClassName(driverConfig.getName())
+                .setURI(driverConfig.getUri());
+        if(driverConfig.getPool().getSize() != 0)
+            config.driverConfiguration().setConnectionPoolSize(driverConfig.getPool().getSize());
+        config.autoIndexConfiguration().setAutoIndex(driverConfig.getIndex());
+
+        return config;
     }
 }
