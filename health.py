125b0e7174647b567a24f25ca38010b960a572c7
Thomas Andolf <thomas.andolf@gmail.com>
2017-06-17 20:18:30 +0200

    changed error codes to 1 when failed exits in health.py

--- health.py
+++ health.py
@@ -6,7 +6,7 @@ try:
     r = requests.get(url)
 except requests.exceptions.ConnectionError as e:
     print('Could not connect to "%s", reason: "%s"' % (str(sys.argv), str(e)))
-    exit(9)
+    exit(1)
 
 if r.status_code == 200:
     j = r.json()
@@ -19,7 +19,7 @@ if r.status_code == 200:
             print('Health check did not pass. Current application status: "%s"' % str(status))
     except KeyError as e:
         print('Key "%s" not defined in json response: "%s"' % (str(e), str(j)))
-        exit(9)
+        exit(1)
 else:
     print('Could not connect to "%s" return status was: "%s"' % (str(sys.argv), str(r.status_code)))
-    exit(-9)
+    exit(1)
